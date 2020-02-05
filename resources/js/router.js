018f6ba5469d9cc2b5f36c37fc3de13acde16bd2
vagrant <vagrant@homestead>
2020-02-05 02:46:47 +0000

    part10 写真投稿フォーム

--- resources/js/router.js
+++ resources/js/router.js
@@ -7,6 +7,7 @@ import Login from './pages/Login.vue'
 
 import store from './store'
 import SystemError from './pages/errors/System.vue'
+import PhotoDetail from './pages/PhotoDetail.vue'
 
 // VueRouterプラグインを使用する
 // これによって<RouterView />コンポーネントなどを使うことができる
@@ -17,6 +18,11 @@ const routes = [
   {
     path: '/',
     component: PhotoList
+  },
+  {
+    path: '/photos/:id',
+    component: PhotoDetail,
+    props: true
   },
   {
     path: '/500',
